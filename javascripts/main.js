c26a56e1368c1b25aabb015099479291c09d1155
Jean-Pascal THIERY <jpascal@lmarin.org>
2015-01-27 16:59:04 +0100

    Create gh-pages branch via GitHub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,1 @@
-jQuery(document).ready(function() {
-  jQuery(".solution").wrap('<div class = "spoiler"></div>')
-  jQuery(".solution").before('<button  type="button" class = "teaser">Afficher la solution</button>')
-  jQuery(".solution").hide();
-  jQuery(document).on('click', '.teaser', function() {
-    jQuery(this).next(".solution").slideToggle(500);
-  });
-});
+console.log('This would be the main JS file.');
