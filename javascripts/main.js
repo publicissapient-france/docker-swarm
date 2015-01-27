5b159892208d47dbc37a0ec920b8a05bf3b0240d
Jean-Pascal Thiery <jthiery.prestataire@voyages-sncf.com>
2015-01-27 17:02:02 +0100

    Revert "Create gh-pages branch via GitHub"
    This reverts commit c26a56e1368c1b25aabb015099479291c09d1155.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+jQuery(document).ready(function() {
+  jQuery(".solution").wrap('<div class = "spoiler"></div>')
+  jQuery(".solution").before('<button  type="button" class = "teaser">Afficher la solution</button>')
+  jQuery(".solution").hide();
+  jQuery(document).on('click', '.teaser', function() {
+    jQuery(this).next(".solution").slideToggle(500);
+  });
+});
