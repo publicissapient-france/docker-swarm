27d0508dee36f7a9fe446cc37da01960d6993d5f
Jonathan Raffre <nk@nyuu.eu>
2015-12-09 21:38:14 +0100

    update terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,12 @@
 output "docker_manager_url" {
-    value = "${aws_instance.docker_manager_1.public_ip}"
+    value = "tcp://${aws_instance.docker_manager_1.public_ip}:2375"
 }
 output "docker_slave_1_url" {
-    value = "${aws_instance.docker_slave_1.public_ip}"
+    value = "tcp://${aws_instance.docker_slave_1.public_ip}:2375"
 }
 output "docker_slave_2_url" {
-    value = "${aws_instance.docker_slave_2.public_ip}"
+    value = "tcp://${aws_instance.docker_slave_2.public_ip}:2375"
 }
 output "docker_slave_3_url" {
-    value = "${aws_instance.docker_slave_3.public_ip}"
+    value = "tcp://${aws_instance.docker_slave_3.public_ip}:2375"
 }
